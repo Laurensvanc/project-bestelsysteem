50ff7d2d197936ff1c2f6d4acdd533850cbf6294
Laurens <laurensvanc@hotmail.com>
2021-05-24 13:58:32 +0200

    Added ui navigation bar 😬 + navigation bar + Order_productDAO query to collect Order_Product productID and Amount

---
+++ ChapooModel/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChapooModel
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public int ProductID { get; set; }
+        public int Aantal { get; set; }
+    
+        public Order(int orderId, int aantal)
+        {
+            OrderID = orderId;
+            Aantal = aantal;
+        }
+    }
+}
---
+++ Login/Menu.Designer.cs
@@ -0,0 +1,160 @@
+﻿
+namespace Login
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.btn_navHome = new System.Windows.Forms.ToolStripButton();
+            this.btn_navReserveringen = new System.Windows.Forms.ToolStripButton();
+            this.btn_navBarKeuken = new System.Windows.Forms.ToolStripButton();
+            this.btn_navBestellingen = new System.Windows.Forms.ToolStripButton();
+            this.btn_navVoorraad = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(99)))), ((int)(((byte)(151)))));
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton2,
+            this.btn_navHome,
+            this.btn_navReserveringen,
+            this.btn_navBarKeuken,
+            this.btn_navBestellingen,
+            this.btn_navVoorraad});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(150, 450);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(147, 20);
+            this.toolStripButton2.Text = "Logo";
+            // 
+            // btn_navHome
+            // 
+            this.btn_navHome.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(61)))), ((int)(((byte)(102)))));
+            this.btn_navHome.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btn_navHome.Font = new System.Drawing.Font("Roboto", 16F);
+            this.btn_navHome.ForeColor = System.Drawing.Color.White;
+            this.btn_navHome.Image = ((System.Drawing.Image)(resources.GetObject("btn_navHome.Image")));
+            this.btn_navHome.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btn_navHome.Name = "btn_navHome";
+            this.btn_navHome.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btn_navHome.Size = new System.Drawing.Size(147, 31);
+            this.btn_navHome.Text = "Home";
+            this.btn_navHome.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_navHome.ToolTipText = "toolStripButton1";
+            // 
+            // btn_navReserveringen
+            // 
+            this.btn_navReserveringen.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btn_navReserveringen.Font = new System.Drawing.Font("Roboto", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_navReserveringen.ForeColor = System.Drawing.Color.White;
+            this.btn_navReserveringen.Image = ((System.Drawing.Image)(resources.GetObject("btn_navReserveringen.Image")));
+            this.btn_navReserveringen.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btn_navReserveringen.Name = "btn_navReserveringen";
+            this.btn_navReserveringen.Size = new System.Drawing.Size(147, 29);
+            this.btn_navReserveringen.Text = "Reserveringen";
+            this.btn_navReserveringen.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btn_navBarKeuken
+            // 
+            this.btn_navBarKeuken.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(61)))), ((int)(((byte)(102)))));
+            this.btn_navBarKeuken.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btn_navBarKeuken.Font = new System.Drawing.Font("Roboto", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_navBarKeuken.ForeColor = System.Drawing.Color.White;
+            this.btn_navBarKeuken.Image = ((System.Drawing.Image)(resources.GetObject("btn_navBarKeuken.Image")));
+            this.btn_navBarKeuken.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btn_navBarKeuken.Name = "btn_navBarKeuken";
+            this.btn_navBarKeuken.Size = new System.Drawing.Size(147, 29);
+            this.btn_navBarKeuken.Text = "Bar/Keuken";
+            this.btn_navBarKeuken.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btn_navBestellingen
+            // 
+            this.btn_navBestellingen.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btn_navBestellingen.Font = new System.Drawing.Font("Roboto", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_navBestellingen.ForeColor = System.Drawing.Color.White;
+            this.btn_navBestellingen.Image = ((System.Drawing.Image)(resources.GetObject("btn_navBestellingen.Image")));
+            this.btn_navBestellingen.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btn_navBestellingen.Name = "btn_navBestellingen";
+            this.btn_navBestellingen.Size = new System.Drawing.Size(147, 29);
+            this.btn_navBestellingen.Text = "Bestellingen";
+            this.btn_navBestellingen.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btn_navVoorraad
+            // 
+            this.btn_navVoorraad.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(61)))), ((int)(((byte)(102)))));
+            this.btn_navVoorraad.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btn_navVoorraad.Font = new System.Drawing.Font("Roboto", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_navVoorraad.ForeColor = System.Drawing.Color.White;
+            this.btn_navVoorraad.Image = ((System.Drawing.Image)(resources.GetObject("btn_navVoorraad.Image")));
+            this.btn_navVoorraad.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_navVoorraad.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btn_navVoorraad.Name = "btn_navVoorraad";
+            this.btn_navVoorraad.Size = new System.Drawing.Size(147, 29);
+            this.btn_navVoorraad.Text = "Voorraad";
+            this.btn_navVoorraad.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Menu";
+            this.Text = "Chapoo systeem";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton btn_navHome;
+        private System.Windows.Forms.ToolStripButton btn_navReserveringen;
+        private System.Windows.Forms.ToolStripButton btn_navBarKeuken;
+        private System.Windows.Forms.ToolStripButton btn_navBestellingen;
+        private System.Windows.Forms.ToolStripButton btn_navVoorraad;
+    }
+}
