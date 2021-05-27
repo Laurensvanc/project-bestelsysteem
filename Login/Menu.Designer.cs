c25672967c18c16be274e77ea70967350318acff
TimRof <70700788+TimRof@users.noreply.github.com>
2021-05-28 00:15:50 +0200

    Bug Fixed

--- Login/Menu.Designer.cs
+++ Login/Menu.Designer.cs
@@ -31,17 +31,12 @@ namespace Login
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
             this.ts_navigation = new System.Windows.Forms.ToolStrip();
-            this.lbl_userDisplay = new System.Windows.Forms.Label();
-            this.lbl_timeDisplay = new System.Windows.Forms.Label();
-            this.pnl_Display = new System.Windows.Forms.Panel();
-            this.pnl_WhiteStrip = new System.Windows.Forms.Panel();
-            this.pnl_PinkStrip = new System.Windows.Forms.Panel();
-            this.btn_closeForm = new System.Windows.Forms.Button();
             this.btn_navLogo = new System.Windows.Forms.ToolStripButton();
             this.btn_navHome = new System.Windows.Forms.ToolStripButton();
             this.btn_navReserveringen = new System.Windows.Forms.ToolStripButton();
             this.btn_navBarKeuken = new System.Windows.Forms.ToolStripButton();
             this.btn_navBestellingen = new System.Windows.Forms.ToolStripButton();
+            this.btn_navAfrekenen = new System.Windows.Forms.ToolStripButton();
             this.btn_navVoorraad = new System.Windows.Forms.ToolStripButton();
             this.btn_navRegistreren = new System.Windows.Forms.ToolStripButton();
             this.lbl_userDisplay = new System.Windows.Forms.Label();
@@ -52,10 +47,8 @@ namespace Login
             this.pnl_PinkStrip = new System.Windows.Forms.Panel();
             this.pnl_LogoutBorder = new System.Windows.Forms.Panel();
             this.pnl_Logout = new System.Windows.Forms.Panel();
-            this.btn_Uitloggen = new System.Windows.Forms.Button();
             this.btn_Afsluiten = new System.Windows.Forms.Button();
-            this.btn_navAfrekenen = new System.Windows.Forms.ToolStripButton();
-            this.btn_navRegistreren = new System.Windows.Forms.ToolStripButton();
+            this.btn_Uitloggen = new System.Windows.Forms.Button();
             this.ts_navigation.SuspendLayout();
             this.pnl_WhiteStrip.SuspendLayout();
             this.pnl_LogoutBorder.SuspendLayout();
@@ -82,69 +75,6 @@ namespace Login
             this.ts_navigation.Size = new System.Drawing.Size(344, 1080);
             this.ts_navigation.TabIndex = 0;
             this.ts_navigation.Text = "toolStrip1";
-            // 
-            // lbl_userDisplay
-            // 
-            this.lbl_userDisplay.AutoSize = true;
-            this.lbl_userDisplay.BackColor = System.Drawing.Color.Transparent;
-            this.lbl_userDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_userDisplay.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(112)))), ((int)(((byte)(112)))));
-            this.lbl_userDisplay.Location = new System.Drawing.Point(44, 19);
-            this.lbl_userDisplay.Name = "lbl_userDisplay";
-            this.lbl_userDisplay.Size = new System.Drawing.Size(176, 31);
-            this.lbl_userDisplay.TabIndex = 2;
-            this.lbl_userDisplay.Text = "Logged in as:";
-            // 
-            // lbl_timeDisplay
-            // 
-            this.lbl_timeDisplay.AutoSize = true;
-            this.lbl_timeDisplay.BackColor = System.Drawing.Color.Transparent;
-            this.lbl_timeDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_timeDisplay.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(112)))), ((int)(((byte)(112)))));
-            this.lbl_timeDisplay.Location = new System.Drawing.Point(1106, 19);
-            this.lbl_timeDisplay.Name = "lbl_timeDisplay";
-            this.lbl_timeDisplay.Size = new System.Drawing.Size(0, 31);
-            this.lbl_timeDisplay.TabIndex = 3;
-            // 
-            // pnl_Display
-            // 
-            this.pnl_Display.Location = new System.Drawing.Point(344, 138);
-            this.pnl_Display.Name = "pnl_Display";
-            this.pnl_Display.Size = new System.Drawing.Size(1576, 942);
-            this.pnl_Display.TabIndex = 4;
-            // 
-            // pnl_WhiteStrip
-            // 
-            this.pnl_WhiteStrip.Controls.Add(this.btn_closeForm);
-            this.pnl_WhiteStrip.Controls.Add(this.lbl_userDisplay);
-            this.pnl_WhiteStrip.Controls.Add(this.lbl_timeDisplay);
-            this.pnl_WhiteStrip.Location = new System.Drawing.Point(343, 0);
-            this.pnl_WhiteStrip.Name = "pnl_WhiteStrip";
-            this.pnl_WhiteStrip.Size = new System.Drawing.Size(1577, 69);
-            this.pnl_WhiteStrip.TabIndex = 5;
-            // 
-            // pnl_PinkStrip
-            // 
-            this.pnl_PinkStrip.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(99)))), ((int)(((byte)(151)))));
-            this.pnl_PinkStrip.Location = new System.Drawing.Point(338, 69);
-            this.pnl_PinkStrip.Name = "pnl_PinkStrip";
-            this.pnl_PinkStrip.Size = new System.Drawing.Size(1600, 69);
-            this.pnl_PinkStrip.TabIndex = 6;
-            // 
-            // btn_closeForm
-            // 
-            this.btn_closeForm.BackColor = System.Drawing.Color.Transparent;
-            this.btn_closeForm.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btn_closeForm.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_closeForm.ForeColor = System.Drawing.Color.Transparent;
-            this.btn_closeForm.Image = global::Login.Properties.Resources.Shut_down;
-            this.btn_closeForm.Location = new System.Drawing.Point(1517, 9);
-            this.btn_closeForm.Name = "btn_closeForm";
-            this.btn_closeForm.Size = new System.Drawing.Size(44, 49);
-            this.btn_closeForm.TabIndex = 1;
-            this.btn_closeForm.TabStop = false;
-            this.btn_closeForm.UseVisualStyleBackColor = false;
-            this.btn_closeForm.Click += new System.EventHandler(this.btn_closeForm_Click);
             // 
             // btn_navLogo
             // 
@@ -215,6 +145,20 @@ namespace Login
             this.btn_navBestellingen.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btn_navBestellingen.Click += new System.EventHandler(this.btn_navBestellingen_Click);
             // 
+            // btn_navAfrekenen
+            // 
+            this.btn_navAfrekenen.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(61)))), ((int)(((byte)(102)))));
+            this.btn_navAfrekenen.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btn_navAfrekenen.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F);
+            this.btn_navAfrekenen.ForeColor = System.Drawing.Color.White;
+            this.btn_navAfrekenen.Image = ((System.Drawing.Image)(resources.GetObject("btn_navAfrekenen.Image")));
+            this.btn_navAfrekenen.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btn_navAfrekenen.Name = "btn_navAfrekenen";
+            this.btn_navAfrekenen.Size = new System.Drawing.Size(342, 59);
+            this.btn_navAfrekenen.Text = "Afrekenen";
+            this.btn_navAfrekenen.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_navAfrekenen.Click += new System.EventHandler(this.btn_navAfrekenen_Click);
+            // 
             // btn_navVoorraad
             // 
             this.btn_navVoorraad.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(99)))), ((int)(((byte)(151)))));
@@ -231,13 +175,14 @@ namespace Login
             // 
             // btn_navRegistreren
             // 
+            this.btn_navRegistreren.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(61)))), ((int)(((byte)(102)))));
             this.btn_navRegistreren.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.btn_navRegistreren.Font = new System.Drawing.Font("Roboto", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_navRegistreren.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F);
             this.btn_navRegistreren.ForeColor = System.Drawing.Color.White;
             this.btn_navRegistreren.Image = ((System.Drawing.Image)(resources.GetObject("btn_navRegistreren.Image")));
             this.btn_navRegistreren.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.btn_navRegistreren.Name = "btn_navRegistreren";
-            this.btn_navRegistreren.Size = new System.Drawing.Size(342, 62);
+            this.btn_navRegistreren.Size = new System.Drawing.Size(342, 59);
             this.btn_navRegistreren.Text = "Registreren";
             this.btn_navRegistreren.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btn_navRegistreren.Click += new System.EventHandler(this.btn_navRegistreren_Click);
@@ -300,19 +245,12 @@ namespace Login
             this.btn_closeForm.Click += new System.EventHandler(this.btn_closeForm_Click);
             // 
             // pnl_PinkStrip
-            // btn_navAfrekenen
             // 
-            this.btn_navAfrekenen.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(61)))), ((int)(((byte)(102)))));
-            this.btn_navAfrekenen.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.btn_navAfrekenen.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F);
-            this.btn_navAfrekenen.ForeColor = System.Drawing.Color.White;
-            this.btn_navAfrekenen.Image = ((System.Drawing.Image)(resources.GetObject("btn_navAfrekenen.Image")));
-            this.btn_navAfrekenen.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.btn_navAfrekenen.Name = "btn_navAfrekenen";
-            this.btn_navAfrekenen.Size = new System.Drawing.Size(342, 59);
-            this.btn_navAfrekenen.Text = "Afrekenen";
-            this.btn_navAfrekenen.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btn_navAfrekenen.Click += new System.EventHandler(this.btn_navAfrekenen_Click);
+            this.pnl_PinkStrip.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(99)))), ((int)(((byte)(151)))));
+            this.pnl_PinkStrip.Location = new System.Drawing.Point(338, 69);
+            this.pnl_PinkStrip.Name = "pnl_PinkStrip";
+            this.pnl_PinkStrip.Size = new System.Drawing.Size(1600, 69);
+            this.pnl_PinkStrip.TabIndex = 6;
             // 
             // pnl_LogoutBorder
             // 
@@ -334,22 +272,6 @@ namespace Login
             this.pnl_Logout.Size = new System.Drawing.Size(134, 116);
             this.pnl_Logout.TabIndex = 5;
             // 
-            // btn_Uitloggen
-            // 
-            this.btn_Uitloggen.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btn_Uitloggen.FlatAppearance.BorderSize = 0;
-            this.btn_Uitloggen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_Uitloggen.Font = new System.Drawing.Font("Roboto", 18F);
-            this.btn_Uitloggen.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(112)))), ((int)(((byte)(112)))));
-            this.btn_Uitloggen.Location = new System.Drawing.Point(0, 0);
-            this.btn_Uitloggen.Name = "btn_Uitloggen";
-            this.btn_Uitloggen.Size = new System.Drawing.Size(134, 58);
-            this.btn_Uitloggen.TabIndex = 1;
-            this.btn_Uitloggen.TabStop = false;
-            this.btn_Uitloggen.Text = "Uitloggen";
-            this.btn_Uitloggen.UseVisualStyleBackColor = true;
-            this.btn_Uitloggen.Click += new System.EventHandler(this.btn_Uitloggen_Click);
-            // 
             // btn_Afsluiten
             // 
             this.btn_Afsluiten.Cursor = System.Windows.Forms.Cursors.Hand;
@@ -365,17 +287,22 @@ namespace Login
             this.btn_Afsluiten.Text = "Afsluiten";
             this.btn_Afsluiten.UseVisualStyleBackColor = true;
             this.btn_Afsluiten.Click += new System.EventHandler(this.btn_Afsluiten_Click);
-            this.btn_navRegistreren.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(61)))), ((int)(((byte)(102)))));
-            this.btn_navRegistreren.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.btn_navRegistreren.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F);
-            this.btn_navRegistreren.ForeColor = System.Drawing.Color.White;
-            this.btn_navRegistreren.Image = ((System.Drawing.Image)(resources.GetObject("btn_navRegistreren.Image")));
-            this.btn_navRegistreren.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.btn_navRegistreren.Name = "btn_navRegistreren";
-            this.btn_navRegistreren.Size = new System.Drawing.Size(342, 59);
-            this.btn_navRegistreren.Text = "Registreren";
-            this.btn_navRegistreren.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btn_navRegistreren.Click += new System.EventHandler(this.btn_navRegistreren_Click);
+            // 
+            // btn_Uitloggen
+            // 
+            this.btn_Uitloggen.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Uitloggen.FlatAppearance.BorderSize = 0;
+            this.btn_Uitloggen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Uitloggen.Font = new System.Drawing.Font("Roboto", 18F);
+            this.btn_Uitloggen.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(112)))), ((int)(((byte)(112)))));
+            this.btn_Uitloggen.Location = new System.Drawing.Point(0, 0);
+            this.btn_Uitloggen.Name = "btn_Uitloggen";
+            this.btn_Uitloggen.Size = new System.Drawing.Size(134, 58);
+            this.btn_Uitloggen.TabIndex = 1;
+            this.btn_Uitloggen.TabStop = false;
+            this.btn_Uitloggen.Text = "Uitloggen";
+            this.btn_Uitloggen.UseVisualStyleBackColor = true;
+            this.btn_Uitloggen.Click += new System.EventHandler(this.btn_Uitloggen_Click);
             // 
             // Menu
             // 
